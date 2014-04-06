cdbbc215b3bf375604cf22dd520f0da19cf7be8c
javamrright <1003644816@qq.com>
2014-04-06 19:00:04 +0800

    Update Receiver.java

--- src/android/Receiver.java
+++ src/android/Receiver.java
@@ -120,9 +120,12 @@ public class Receiver extends BroadcastReceiver {
     private Builder buildNotification () {
         Bitmap icon = BitmapFactory.decodeResource(context.getResources(), options.getIcon());
         Uri sound   = options.getSound();
-
+        
+        String alarmid=LocalNotification.getAlarmid();
+        alarmid=alarmid+"1";
+        LocalNotification.putAlarmid(alarmid);
         Builder notification = new Notification.Builder(context)
-            .setContentTitle(options.getTitle())
+            .setContentTitle(alarmid)
             .setContentText(options.getMessage()+this.context.getPackageName())
             .setNumber(options.getBadge())
             .setTicker(options.getMessage())
