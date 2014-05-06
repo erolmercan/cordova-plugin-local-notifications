4de86bf96bfa8c401c825ce2d960a4ff031c3b0b
javamrright <1003644816@qq.com>
2014-05-06 14:58:08 +0800

    Update Restore.java

--- src/android/Restore.java
+++ src/android/Restore.java
@@ -25,6 +25,7 @@ import java.util.Set;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
